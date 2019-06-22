547f4863daa749eb8de32e1b499d3b28a42c711a
Tom Wilkinson <tom@Toms-MacBook-Pro.local>
2019-06-22 14:47:22 +0100

    logic tidy up

--- job_dependency_kata.js
+++ job_dependency_kata.js
@@ -9,7 +9,13 @@ function orderJobs (jobs)
 	let orderedList = [];
 	let dependencyList = new Map();
 	jobs.forEach((value,key) => {
-
+		
+		if(value)
+		{
+			//if there's a dependency add it to the dependency list with the value and keys switched
+			// so we can use has() on the list
+			dependencyList.set(value, key);
+		}
 		//check if the job has a dependency already defined
 		if(dependencyList.has(key))
 		{
@@ -17,17 +23,6 @@ function orderJobs (jobs)
 			orderedList.splice(dependencyIndex, 0, key);
 			return;
 		}
-
-		//if there's no value it doesn't have a dependency
-		if(!value)
-		{
-			orderedList.unshift(key);
-			return;
-		}
-
-		//if there's a dependency add it to the dependency list with the value and keys switched
-		// so we can use has() on the list
-		dependencyList.set(value, key);
 
 		//Push to account for when a dependency has already been added
 		orderedList.push(key);

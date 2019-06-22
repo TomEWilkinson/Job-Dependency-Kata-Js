7937015be675b1f5e5086f598d108ad7bdac36df
Tom Wilkinson <tom@Toms-MacBook-Pro.local>
2019-06-22 10:59:47 +0100

    Test when on job without a dependency is passed in ("a => ''")

--- job_dependency_kata.test.js
+++ job_dependency_kata.test.js
@@ -3,3 +3,9 @@ const order_jobs = require("./job_dependency_kata");
 test("Passing an empty string of jobs returns an empty list", () =>{
 	expect(order_jobs("")).toBe("");
 });
+
+test("Passing an empty string of jobs returns an empty list", () =>{
+	var jobs = new Map();
+	jobs.set("a", "");
+	expect(order_jobs(jobs)).toBe(["a"]);
+});

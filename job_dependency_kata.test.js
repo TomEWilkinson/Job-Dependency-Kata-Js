d13c2cf66230e7df5bb5f185a239e37de1a31cb9
Tom Wilkinson <tom@Toms-MacBook-Pro.local>
2019-06-22 12:08:46 +0100

    added test for 3 jobs with one dependency

--- job_dependency_kata.test.js
+++ job_dependency_kata.test.js
@@ -19,3 +19,13 @@ test("Passing an map of three jobs with no dependcies returns those jobs in any 
 	expect(order_jobs(jobs)).toContain("b");
 	expect(order_jobs(jobs)).toContain("c");
 });
+
+test("Passing an map of three jobs with dependcies returns those jobs where  c before b", () =>{
+	var jobs = new Map();
+	jobs.set("a", "");
+	jobs.set("b", "c");
+	jobs.set("c", "");
+
+	var ordered_jobs = order_jobs(jobs);
+	expect(ordered_jobs.indexOf("c") > ordered_jobs.indexOf("b")).toBeTruthy();
+});

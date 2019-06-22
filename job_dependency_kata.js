11671002be030564b3bdeb36011b70f14f5c378a
Tom Wilkinson <tom@Toms-MacBook-Pro.local>
2019-06-22 15:11:08 +0100

    Check for circle dependencies

--- job_dependency_kata.js
+++ job_dependency_kata.js
@@ -20,7 +20,14 @@ function orderJobs (jobs)
 			//if there's a dependency add it to the dependency list with the value and keys switched
 			// so we can use has() on the list
 			dependencyList.set(value, key);
+			
+			//if the key is already in the dependency list there's a possibility for circular dependencies
+			if(dependencyList.has(key))
+			{
+				circleDependencyCheck(dependencyList, dependencyList.get(key), key);
+			}
 		}
+
 		//check if the job has a dependency already defined
 		if(dependencyList.has(key))
 		{
@@ -35,6 +42,23 @@ function orderJobs (jobs)
 	});
     
 	return orderedList;
+}
+
+function circleDependencyCheck(dependencyList,nextKey,originalKey )
+{
+	//if the orginal key equals the next one we have a circle dependency
+	if(originalKey == dependencyList.get(nextKey))
+	{
+		throw new Error("jobs cannot be dependent on themselves");
+	}
+
+	//if it's not undeifned the chain continues
+	if(dependencyList.get(nextKey) != undefined)
+	{
+		circleDependencyCheck(dependencyList, dependencyList.get(nextKey), originalKey);
+	} else {
+		return false;
+	}
 }
 
 module.exports = orderJobs;

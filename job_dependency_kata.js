775c8adeb2d9a26445dcb1e82ee1938498665b2d
Tom Wilkinson <tom@Toms-MacBook-Pro.local>
2019-06-22 14:18:55 +0100

    implements multiple dependant jobs

--- job_dependency_kata.js
+++ job_dependency_kata.js
@@ -6,8 +6,29 @@ function order_jobs (jobs)
 	}
      
 	let ordered_list = [];
+	let dependency_list = new Map();
 	jobs.forEach((value,key) => {
-		ordered_list.unshift(key);
+
+		//check if the job has a dependency already defined
+		if(dependency_list.has(key))
+		{
+			let dependencyIndex = ordered_list.indexOf(dependency_list.get(key));
+			ordered_list.splice(dependencyIndex, 0, key);
+			return;
+		}
+
+		//if there's no value it doesn't have a dependency
+		if(!value)
+		{
+			ordered_list.unshift(key);
+			return;
+		}
+
+		//if there's a dependency add it to the dependency list with the value and keys switched
+		// so we can use has() on the list
+		dependency_list.set(value, key);
+		ordered_list.push(key);
+
 	});
     
 	return ordered_list;
